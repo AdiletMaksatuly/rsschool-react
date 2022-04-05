2f21bb4fdfd2f10480b793a516d2acafa6bb4dc0
AdiletMaksatuly <bakeryamaker1@gmail.com>
2022-04-05 15:57:13 +0600

    feat: add catching errors and showing error text to user

--- react-components/src/pages/Home.tsx
+++ react-components/src/pages/Home.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import { isError } from 'util';
 import CardList from '../components/CardList';
 import SearchBar from '../components/SearchBar';
 import { ICard } from '../types';
-
 type HomeState = {
   cards: ICard[];
   pageInfo: {
@@ -11,8 +11,11 @@ type HomeState = {
   };
   searchQuery: string;
   prevSearchQuery: string;
+  error: {
+    isError: boolean;
+    message?: string;
+  };
 };
-
 class Home extends React.Component<unknown, HomeState> {
   state: HomeState = {
     cards: [],
@@ -22,65 +25,69 @@ class Home extends React.Component<unknown, HomeState> {
       totalPages: 1,
       currentPage: 1,
     },
+    error: {
+      isError: false,
+    },
   };
-
   constructor(props: unknown) {
     super(props);
   }
-
   componentDidMount() {
     this.fetchCharacters();
   }
-
   fetchCharacters = async () => {
     try {
       const response = await fetch(
         `https://rickandmortyapi.com/api/character/?page=${this.state.pageInfo.currentPage}&name=${this.state.searchQuery}`
       );
-
-      const { results: cardsData, info } = await response.json();
-
-      console.log(cardsData);
-
-      this.setState({
-        cards: cardsData,
-        pageInfo: { ...this.state.pageInfo, totalPages: info.pages },
-      });
+      if (response.ok) {
+        const { results: cardsData, info } = await response.json();
+        console.log(cardsData);
+        this.setState({
+          cards: cardsData,
+          pageInfo: { ...this.state.pageInfo, totalPages: info.pages },
+        });
+      } else {
+        throw new Error(`${response.status}`);
+      }
     } catch (error) {
-      console.log(error);
+      if ((error as Error).message === '404') {
+        this.setState({ error: { isError: true, message: 'Not found' } });
+      }
     }
   };
-
   searchCharacters = () => {
+    if (this.state.error.isError) {
+      this.setState({ error: { isError: false, message: '' } });
+    }
     this.fetchCharacters();
   };
-
   searchResetHandler = () => {
     this.setState({ searchQuery: '' }, () => this.searchCharacters());
   };
-
   componentDidUpdate = (_: unknown, prevState: HomeState) => {
     if (prevState.searchQuery !== this.state.searchQuery) {
       this.setState({ prevSearchQuery: prevState.searchQuery });
     }
   };
-
   render() {
     return (
       <main className="page">
         <div className="page__content container">
           <h1>Home page</h1>
           <SearchBar
-            // onChange={this.updateSearchQuery}
             onChange={(value) => this.setState({ searchQuery: value })}
             onReset={this.searchResetHandler}
             onSubmit={this.searchCharacters}
           />
-          <CardList cards={this.state.cards} />
+          {this.state.error.isError ? (
+            <div>Not found... Try to enter another search query</div>
+          ) : (
+            <CardList cards={this.state.cards} />
+          )}
         </div>
       </main>
     );
   }
 }
-
 export default Home;
