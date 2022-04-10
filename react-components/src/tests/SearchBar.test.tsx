4c3e57747910e8b4faca72f61f32c2fd3c68f0fe
AdiletMaksatuly <bakeryamaker1@gmail.com>
2022-04-10 13:42:49 +0600

    fix: edit searchbar testing

--- react-components/src/tests/SearchBar.test.tsx
+++ react-components/src/tests/SearchBar.test.tsx
@@ -1,28 +1,13 @@
 import { screen } from '@testing-library/react';
 import { renderWithRouter } from '../helpers/renderWithRouter';
 import userEvent from '@testing-library/user-event';
-import Navbar from '../components/Navbar';
-import { findLinkFromNavbar } from './utils';
-import { fakeLocalStorage } from './mocks';
 import Home from '../pages/Home';
 
-window.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () => Promise.resolve({ rates: { CAD: 1.42 } }),
-  })
-) as jest.Mock;
-
 describe('searchbar testing', () => {
-  beforeAll(() => {
-    Object.defineProperty(window, 'localStorage', {
-      value: fakeLocalStorage,
-    });
-  });
-
   test('searchbar value getting from local storage when mounting and setting when unmounting', () => {
     const { unmount } = renderWithRouter(<Home />);
 
-    expect(localStorage.getItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.getItem).toHaveBeenCalledWith('searchQuery');
 
     const searchInput = screen.getByRole('searchbox');
 
@@ -33,8 +18,6 @@ describe('searchbar testing', () => {
 
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(localStorage.setItem).toHaveBeenCalledWith('searchQuery', randomTextUserWillType);
-
-    // ошибка
-    // expect(localStorage.getItem('searchQuery')).toBe(randomTextUserWillType);
+    expect(localStorage.getItem('searchQuery')).toBe(randomTextUserWillType);
   });
 });
