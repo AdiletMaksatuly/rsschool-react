fe600355caf46f5edacac0edc0195f21f2b866c8
AdiletMaksatuly <bakeryamaker1@gmail.com>
2022-04-12 23:38:42 +0600

    refactor: UserCard refactor to hooks

--- react-components/src/components/CardItem/UserCard.tsx
+++ react-components/src/components/CardItem/UserCard.tsx
@@ -12,39 +12,33 @@ type ICountries = {
   ua: 'Ukraine';
 };
 
-class UserCard extends React.Component<UserCardProps> {
-  countries: ICountries = {
+const UserCard: React.FC<UserCardProps> = ({ card }) => {
+  const countries: ICountries = {
     kz: 'Kazakhstan',
     ru: 'Russia',
     ua: 'Ukraine',
   };
 
-  constructor(props: UserCardProps) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <li className={classes['card-item']} data-testid="cardItem">
-        <article className={classes['card']}>
-          <img
-            src={
-              `${this.props.card.img}` ||
-              'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
-            }
-            className={classes['card__img']}
-            alt={this.props.card.name}
-            width="300px"
-            height="300px"
-          />
-          <h3 className={classes['card__title']}>{this.props.card.name}</h3>
-          <p>Date of Birth: {this.props.card.birthDate}</p>
-          <p>Birth place: {this.countries[this.props.card.birthPlace as keyof ICountries]}</p>
-          <p>Sex: {this.props.card.sex}</p>
-        </article>
-      </li>
-    );
-  }
-}
+  return (
+    <li className={classes['card-item']} data-testid="cardItem">
+      <article className={classes['card']}>
+        <img
+          src={
+            `${card.img}` ||
+            'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
+          }
+          className={classes['card__img']}
+          alt={card.name}
+          width="300px"
+          height="300px"
+        />
+        <h3 className={classes['card__title']}>{card.name}</h3>
+        <p>Date of Birth: {card.birthDate}</p>
+        <p>Birth place: {countries[card.birthPlace as keyof ICountries]}</p>
+        <p>Sex: {card.sex}</p>
+      </article>
+    </li>
+  );
+};
 
 export default UserCard;
