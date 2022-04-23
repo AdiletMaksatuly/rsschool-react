c98a4dc06e78bb1772b3286ac0888af4ebbbe0cb
AdiletMaksatuly <bakeryamaker1@gmail.com>
2022-04-23 15:05:41 +0600

    fix: delete request for character and replace with getting it from state

--- react-components/src/pages/CharacterPage/CharacterPage.tsx
+++ react-components/src/pages/CharacterPage/CharacterPage.tsx
@@ -1,21 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { RootContext } from '../../context';
 import { ICard } from '../../types';
 import classes from './CharacterPage.module.css';
 
 const CharacterPage = () => {
   const navigate = useNavigate();
-  const [card, setCard] = useState<ICard>();
   const params = useParams();
 
-  useEffect(() => {
-    const fetchCharacter = async () => {
-      const response = await fetch(`https://rickandmortyapi.com/api/character/${params.id}`);
-      const character = await response.json();
-      setCard(character);
-    };
+  const [state, dispatch] = useContext(RootContext);
+  const { cards } = state;
+  const card = cards.find((card) => {
+    if (params.id) {
+      return card.id === +params.id;
+    }
+  });
 
-    fetchCharacter();
+  useEffect(() => {
+    if (!card) navigate('/');
   }, []);
 
   return (
@@ -32,33 +34,33 @@ const CharacterPage = () => {
             className={[classes['card'], classes['card']].join(' ')}
           >
             <img
-              src={card.image}
+              src={card?.image}
               className={classes['card__image']}
-              alt={card.name}
+              alt={card?.name}
               width="300px"
               height="300px"
             />
             <div className={classes['card__info']}>
-              <h3 className={classes['card__title']}>{card.name}</h3>
+              <h3 className={classes['card__title']}>{card?.name}</h3>
               <p
                 className={[
                   classes['card__status'],
-                  card.status === 'Alive'
+                  card?.status === 'Alive'
                     ? classes['card__status--alive']
                     : classes['card__status--dead'],
                 ].join(' ')}
               >
-                {card.status} - {card.species}
+                {card?.status} - {card?.species}
               </p>
 
               <p className={classes['card__text']}>
                 Last known location:
                 <br />
-                <span>{card.location.name}</span>
+                <span>{card?.location.name}</span>
               </p>
               <p className={classes['card__text']}>
                 Gender: <br />
-                <span>{card.gender}</span>
+                <span>{card?.gender}</span>
               </p>
             </div>
           </article>
